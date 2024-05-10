b4b674a10203d11efdeffb778b0c11fe24d96650
Mohamed Ahmed <m.ahmed20201863@gmail.com>
2024-05-11 00:35:22 +0300

    finish nots button sheets

--- lib/views/widgets/add_note_bottom_sheet.dart
+++ lib/views/widgets/add_note_bottom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/constants.dart';
+import 'package:notes_app/views/widgets/custom_button.dart';
 import 'package:notes_app/views/widgets/custom_text_Field.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
@@ -8,24 +10,33 @@ class AddNoteBottomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.only(left: 16, right: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            label: 'title',
-            hint: 'Enter your title',
-          ),
-          SizedBox(
-            height: 20,
-          ),
-          CustomTextField(
-            label: 'content',
-            hint: '',
-            maxlines: 7,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              label: 'title',
+              hint: 'Enter your title',
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomTextField(
+              label: 'content',
+              hint: 'content',
+              maxlines: 6,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 20,
+            ),
+          ],
+        ),
       ),
     );
   }
