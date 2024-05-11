79402b02f221876764dd7e423e035357e2213cda
Mohamed Ahmed <m.ahmed20201863@gmail.com>
2024-05-11 16:22:45 +0300

    create edite note view

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -1,60 +1,74 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/edit_note_view.dart';
 
 class CustomNoteItem extends StatelessWidget {
   const CustomNoteItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 4, bottom: 4),
-      child: Container(
-        padding: const EdgeInsets.only(top: 22, bottom: 22),
-        decoration: BoxDecoration(
-          color: const Color.fromARGB(255, 223, 172, 63),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Padding(
-                padding: EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Flutter Tips',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 35,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) {
+              return const EditNoteView();
+            },
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(top: 4, bottom: 4),
+        child: Container(
+          padding: const EdgeInsets.only(top: 22, bottom: 22),
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 223, 172, 63),
+            borderRadius: BorderRadius.circular(16),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Padding(
+                  padding: EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Flutter Tips',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 35,
+                    ),
                   ),
                 ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(bottom: 16),
+                  child: Text(
+                    'Build your career with mohmaed ahmed ',
+                    style: TextStyle(
+                      color:
+                          const Color.fromARGB(255, 0, 0, 0).withOpacity(0.6),
+                      fontSize: 22,
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.delete,
+                      size: 34,
+                      color: Colors.black,
+                    )),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(bottom: 16),
+              Padding(
+                padding: const EdgeInsets.only(right: 24),
                 child: Text(
-                  'Build your career with mohmaed ahmed ',
+                  'May21,2024',
                   style: TextStyle(
                     color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.6),
-                    fontSize: 22,
                   ),
                 ),
               ),
-              trailing: IconButton(
-                  onPressed: () {},
-                  icon: const Icon(
-                    Icons.delete,
-                    size: 34,
-                    color: Colors.black,
-                  )),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(right: 24),
-              child: Text(
-                'May21,2024',
-                style: TextStyle(
-                  color: const Color.fromARGB(255, 0, 0, 0).withOpacity(0.6),
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
