73d70436cee648591487dc066a85b27e6ecf1210
Mohamed Ahmed <m.ahmed20201863@gmail.com>
2024-05-11 16:43:02 +0300

    add custom appbar to edit note view

--- lib/views/edit_note_view.dart
+++ lib/views/edit_note_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar.dart';
+import 'package:notes_app/views/widgets/custom_text_Field.dart';
 
 class EditNoteView extends StatelessWidget {
   const EditNoteView({super.key});
@@ -7,10 +8,31 @@ class EditNoteView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: Column(
-        children: [
-          CustomAppBar(),
-        ],
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 24),
+        child: Column(
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            CustomAppBar(
+              title: 'Edit Note ',
+              icon: Icons.check,
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            CustomTextField(hint: 'Edit your title ', label: 'title'),
+            SizedBox(
+              height: 30,
+            ),
+            CustomTextField(
+              hint: 'Edit your content ',
+              label: 'content',
+              maxlines: 7,
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/views/widgets/custom_app_bar.dart
+++ lib/views/widgets/custom_app_bar.dart
@@ -2,18 +2,21 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
-
+  const CustomAppBar({super.key, required this.title, required this.icon});
+  final String title;
+  final IconData icon;
   @override
   Widget build(BuildContext context) {
-    return const Row(
+    return Row(
       children: [
         Text(
-          'Notes',
-          style: TextStyle(fontSize: 35),
+          title,
+          style: const TextStyle(fontSize: 35),
         ),
         Spacer(),
-        CustomSearchIcon(),
+        CustomIcon(
+          icon: icon,
+        ),
       ],
     );
   }
