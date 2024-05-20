006330fe50b90ab0c45fa9405ec2634996ab1f19
Mohamed Ahmed <m.ahmed20201863@gmail.com>
2024-05-20 21:33:55 +0300

    add add note to add note bottom sheet

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -13,7 +13,7 @@ class AddNoteCubit extends Cubit<AddNoteState> {
     emit(AddNoteLoading());
     try {
       var notesBox = Hive.box<NoteModel>(kNotesBox);
-      await notesBox.add(note);
+
       emit(AddNoteSuccess());
     } catch (e) {
       emit(AddNotefailure(e.toString()));
